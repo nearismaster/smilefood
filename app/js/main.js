234a0c04551d9ce694a9e8f628592c5e51b18502
nearismaster <nearis@bk.ru>
2024-02-22 19:53:37 +0300

    added header, about, products, footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function () {
-   
+
+   var mixer = mixitup('.products__holder-wrap');
+
+   $('.products__btn').first().click();
+
+   $(document).on('click', '.products__btn', function () {
+      $('.products__btn').removeClass('mixitup-control-active');
+      $(this).addClass('mixitup-control-active');
+   });
+
 });
