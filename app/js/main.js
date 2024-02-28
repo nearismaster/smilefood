0a6249aeb85882e14089151095f998ee58966e0a
nearismaster <nearis@bk.ru>
2024-02-28 16:33:45 +0300

    edits

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,37 @@
 $(function () {
 
-   var mixer = mixitup('.products__holder-wrap');
-
-   $('.products__btn').first().click();
-
-   $(document).on('click', '.products__btn', function () {
-      $('.products__btn').removeClass('mixitup-control-active');
-      $(this).addClass('mixitup-control-active');
+   // Добавляем липкую шапку при скролле
+   $(window).scroll(function () {
+      var stickyHeaderTop = $('.header__sticky').offset().top;
+      if ($(window).scrollTop() > stickyHeaderTop) {
+         $('.header__sticky').addClass('sticky');
+      } else {
+         $('.header__sticky').removeClass('sticky');
+      }
    });
 
+   // Функция для изменения цвета фона при скролле
+   function changeBackgroundColor() {
+      var scrollPosition = $(window).scrollTop(); // Получаем текущую позицию скролла
+
+      // Условие: если позиция скролла больше 60px, добавляем background-color, иначе удаляем
+      if (scrollPosition > 60) {
+         $('.header__sticky').css('background-color', '#fff');
+         $('.header__sticky').css('padding', '15px');
+         $('.header__sticky').css('border-bottom', '1px solid #ff6838');
+         if ($(window).width() >= 1200) {
+            $('.header__sticky .menu').css('padding-top', '0');
+         }
+      } else {
+         $('.header__sticky').css('background-color', 'transparent');
+         $('.header__sticky').css('padding-top', '60px');
+         $('.header__sticky').css('border-bottom', 'transparent');
+      }
+   }
+
+   // Обработчик события скролла для изменения цвета фона
+   $(window).scroll(changeBackgroundColor);
+
+
+   var mixer = mixitup('.products__holder-wrap');
 });
--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,9 @@ const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
 const del = require('del');
 const browserSync = require('browser-sync').create();
+const svgSprite = require('gulp-svg-sprite');
+const cheerio = require('gulp-cheerio');
+const replace = require('gulp-replace');
 
 function browsersync() {
    browserSync.init({
@@ -58,6 +61,30 @@ function images() {
       .pipe(dest('dist/images'))
 }
 
+function svgSprites() {
+   return src('app/images/icons/*.svg')
+      .pipe(cheerio({
+         run: ($) => {
+            $("[fill]").removeAttr("fill");
+            $("[stroke]").removeAttr("stroke");
+            $("[style]").removeAttr("style");
+         },
+         parserOptions: { xmlMode: true },
+      })
+      )
+      .pipe(replace('&gt;', '>')) // боремся с заменой символа 
+      .pipe(
+         svgSprite({
+            mode: {
+               stack: {
+                  sprite: '../sprite.svg',
+               },
+            },
+         })
+      )
+      .pipe(dest('app/images'));
+}
+
 function build() {
    return src([
       'app/**/*.html',
@@ -74,7 +101,8 @@ function cleanDist() {
 function watching() {
    watch(['app/scss/**/*.scss'], styles);
    watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-   watch(['app/**/*.html']).on('change', browserSync.reload)
+   watch(['app/images/icons/*.svg'], svgSprites);
+   watch(['app/**/*.html']).on('change', browserSync.reload);
 }
 
 exports.styles = styles;
@@ -82,8 +110,9 @@ exports.scripts = scripts;
 exports.browsersync = browsersync;
 exports.watching = watching;
 exports.images = images;
+exports.svgSprites = svgSprites;
 exports.cleanDist = cleanDist;
 exports.build = series(cleanDist, images, build);
 
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.default = parallel(svgSprites, styles, scripts, browsersync, watching);
 
